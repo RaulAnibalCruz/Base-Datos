d2266b02321dfcc79368f9a828bc5f2f11fd865d
RaulAnibalCruz <raul.cruzet12d1@gmail.com>
2025-11-24 18:46:34 -0300

    commit

--- Bloody_Roar_2.PersistenciaDapper.Test/obj/Debug/net8.0/Bloody_Roar_2.PersistenciaDapper.Test.AssemblyInfo.cs
+++ Bloody_Roar_2.PersistenciaDapper.Test/obj/Debug/net8.0/Bloody_Roar_2.PersistenciaDapper.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bloody_Roar_2.PersistenciaDapper.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d1f31f8a7fd4c5a4bddeded2d086839bce4d85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb1764e2cb22c87f916836a1493f98114220120")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bloody_Roar_2.PersistenciaDapper.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bloody_Roar_2.PersistenciaDapper.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MinimalAPI/obj/Debug/net8.0/MinimalAPI.AssemblyInfo.cs
+++ MinimalAPI/obj/Debug/net8.0/MinimalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d1f31f8a7fd4c5a4bddeded2d086839bce4d85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb1764e2cb22c87f916836a1493f98114220120")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
